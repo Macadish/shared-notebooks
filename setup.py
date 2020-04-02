0267db51359d34139af07051892bef656a93ef8c
Jon <macadphotos@gmail.com>
2020-04-02 21:01:22 +0800

    Added all files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Shared Notebooks',
+    author='Jonathan Teo',
+    license='MIT',
+)
